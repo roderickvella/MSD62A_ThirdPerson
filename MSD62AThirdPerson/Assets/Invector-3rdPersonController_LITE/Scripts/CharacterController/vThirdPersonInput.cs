dd21505188eefe2ca8d89a12fe757d1ff5857df7
roderickvella <55999524+roderickvella@users.noreply.github.com>
2021-10-04 10:39:25 +0200

    Importing Assets
    We created a new project and imported the assets from moodle.

---
+++ MSD62AThirdPerson/Assets/Invector-3rdPersonController_LITE/Scripts/CharacterController/vThirdPersonInput.cs
@@ -0,0 +1,165 @@
+﻿using UnityEngine;
+
+namespace Invector.vCharacterController
+{
+    public class vThirdPersonInput : MonoBehaviour
+    {
+        #region Variables       
+
+        [Header("Controller Input")]
+        public string horizontalInput = "Horizontal";
+        public string verticallInput = "Vertical";
+        public KeyCode jumpInput = KeyCode.Space;
+        public KeyCode strafeInput = KeyCode.None;
+        public KeyCode sprintInput = KeyCode.LeftShift;
+        public bool inputEnabled = true; //added by roderick
+
+        [Header("Camera Input")]
+        public string rotateCameraXInput = "Mouse X";
+        public string rotateCameraYInput = "Mouse Y";
+
+        [HideInInspector] public vThirdPersonController cc;
+        [HideInInspector] public vThirdPersonCamera tpCamera;
+        [HideInInspector] public Camera cameraMain;
+
+        #endregion
+
+        protected virtual void Start()
+        {
+            InitilizeController();
+            InitializeTpCamera();
+        }
+
+        protected virtual void FixedUpdate()
+        {
+            cc.UpdateMotor();               // updates the ThirdPersonMotor methods
+            cc.ControlLocomotionType();     // handle the controller locomotion type and movespeed
+            cc.ControlRotationType();       // handle the controller rotation type
+        }
+
+        protected virtual void Update()
+        {
+            InputHandle();                  // update the input methods
+            cc.UpdateAnimator();            // updates the Animator Parameters
+        }
+
+        public virtual void OnAnimatorMove()
+        {
+            cc.ControlAnimatorRootMotion(); // handle root motion animations 
+        }
+
+        #region Basic Locomotion Inputs
+
+        protected virtual void InitilizeController()
+        {
+            cc = GetComponent<vThirdPersonController>();
+
+            if (cc != null)
+                cc.Init();
+        }
+
+        protected virtual void InitializeTpCamera()
+        {
+            if (tpCamera == null)
+            {
+                tpCamera = FindObjectOfType<vThirdPersonCamera>();
+                if (tpCamera == null)
+                    return;
+                if (tpCamera)
+                {
+                    tpCamera.SetMainTarget(this.transform);
+                    tpCamera.Init();
+                }
+            }
+        }
+
+        protected virtual void InputHandle()
+        {
+            CameraInput();
+
+            //if..statement added by roderick to enable/disable input by the user 
+            if (inputEnabled)  
+            {
+                MoveInput();
+                SprintInput();
+                //StrafeInput(); --we don't need this for our project. 
+                JumpInput();
+            }
+            else
+            {
+                //if input disabled then reset to no movement
+                cc.input.x = 0f;
+                cc.input.z = 0f;
+                cc.Sprint(false);
+            }
+
+            
+        }
+
+        public virtual void MoveInput()
+        {
+            cc.input.x = Input.GetAxis(horizontalInput);
+            cc.input.z = Input.GetAxis(verticallInput);
+        }
+
+        protected virtual void CameraInput()
+        {
+            if (!cameraMain)
+            {
+                if (!Camera.main) Debug.Log("Missing a Camera with the tag MainCamera, please add one.");
+                else
+                {
+                    cameraMain = Camera.main;
+                    cc.rotateTarget = cameraMain.transform;
+                }
+            }
+
+            if (cameraMain)
+            {
+                cc.UpdateMoveDirection(cameraMain.transform);
+            }
+
+            if (tpCamera == null)
+                return;
+
+            var Y = Input.GetAxis(rotateCameraYInput);
+            var X = Input.GetAxis(rotateCameraXInput);
+
+            tpCamera.RotateCamera(X, Y);
+        }
+
+        protected virtual void StrafeInput()
+        {
+            if (Input.GetKeyDown(strafeInput))
+                cc.Strafe();
+        }
+
+        protected virtual void SprintInput()
+        {
+            if (Input.GetKeyDown(sprintInput))
+                cc.Sprint(true);
+            else if (Input.GetKeyUp(sprintInput))
+                cc.Sprint(false);
+        }
+
+        /// <summary>
+        /// Conditions to trigger the Jump animation & behavior
+        /// </summary>
+        /// <returns></returns>
+        protected virtual bool JumpConditions()
+        {
+            return cc.isGrounded && cc.GroundAngle() < cc.slopeLimit && !cc.isJumping && !cc.stopMove;
+        }
+
+        /// <summary>
+        /// Input to trigger the Jump 
+        /// </summary>
+        protected virtual void JumpInput()
+        {
+            if (Input.GetKeyDown(jumpInput) && JumpConditions())
+                cc.Jump();
+        }
+
+        #endregion       
+    }
+}
