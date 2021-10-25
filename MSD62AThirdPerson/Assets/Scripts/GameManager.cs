cd0fdb07f03871ee3c5acccd395af4ce0c21739d
roderickvella <55999524+roderickvella@users.noreply.github.com>
2021-10-25 10:56:42 +0200

    Added PandoraBox; detection for bottom message

--- MSD62AThirdPerson/Assets/Scripts/GameManager.cs
+++ MSD62AThirdPerson/Assets/Scripts/GameManager.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameObject Canvas;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Canvas = GameObject.Find("Canvas");
     }
 
     private void OnEnable()
